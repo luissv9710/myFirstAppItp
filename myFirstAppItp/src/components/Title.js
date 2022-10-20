21ed02cbba45ccf4b4e7f40c044b20ef77e04dc6
asantana9710 <lsantana@upmh.edu.mx>
2022-10-20 14:39:16 -0500

    Primer práctica de View + Text con JS

---
+++ myFirstAppItp/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text} from "react-native";
+import styles from "../styles/styles";
+
+const Title = () => {
+  return (
+    <Text style={styles.title}>Título</Text>
+  );
+};
+
+export default Title;
---
+++ myFirstAppItp/src/styles/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from "react-native";
+import {background, primary} from "./colors";
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: background,
+  },
+  textPrimary: {
+    color: primary,
+  },
+  title:{
+    color: primary,
+    fontSize: 18,
+  }
+});
+
+export default styles;
